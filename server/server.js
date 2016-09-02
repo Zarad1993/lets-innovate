2a67daf07fe8cdf0c8504ca27ec155775d7d7d0d
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-02 07:36:37 +0300

    Server connected and listening to 8000

--- server/server.js
+++ server/server.js
@@ -1,2 +1,21 @@
+"use strict";
 var mongoose = require('mongoose');
-var express = require('expres');
+var express = require('express');
+
+var MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/innovaters';
+
+mongoose.connect(MONGO_URI);
+
+var app = express();
+
+var port = process.env.PORT || 8000;
+
+// start listening on port 8000
+var listener = app.listen(port , function(){
+	console.log('Listening on port ' + listener.address().port); // Listening port
+});
+
+require('./config/middleware.js');
+// Get the routes Done
+
+module.exports = app;
