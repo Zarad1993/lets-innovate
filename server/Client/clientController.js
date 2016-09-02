db0fa9dd67d93fbb9605e2a65fa4741bac935001
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-02 16:18:01 +0300

    MVP for client is done

--- server/Client/clientController.js
+++ server/Client/clientController.js
@@ -1,3 +1,4 @@
+'use strict';
 var Client = require('./clientModel.js');
 var jwt = require('jwt-simple');
 var helpers = require('./../config/helpers.js');
@@ -9,18 +10,22 @@ module.exports = {
 
 		Client.findOne({email : clientEmail})
 			.exec(function(err, client){
-				if(err){
-					helpers.errorHandler('Username or Password is Incorrect',req,res);
+				if(!client){
+					helpers.errorHandler('Email or Password is Incorrect',req,res);
 				} else{
-				    var token = jwt.encode(client, 'secret');
-			        res.setHeader('x-access-token',token);
-			        //modified the response to send the username
-			        //to save it in local stoarge to be accessed late
-			        res.status(200);
-	                res.json({token:token, email: clientEmail});
+         	        Client.comparePassword(password,client.password, res, function(found){
+	    		        if(found){
+	   				       var token = jwt.encode(client, 'secret');
+	     			        res.setHeader('x-access-token',token);
+	     			        //modified the response to send the username
+	     			        //to save it in local stoarge to be accessed late
+	                        res.json({token:token, email: clientEmail});
+	  			        } else {
+	   				       helpers.errorHandler('Wrong Password', req, res);
+	                    }
+	                });
 				}
-
-            })
+            });	
 	}, 
 
 	signup : function(req,res){
@@ -31,7 +36,6 @@ module.exports = {
 			email : clientEmail,
 			password : password
 		});
-
 		if(password && clientEmail){
 			newClient.save(function(err,saved){
 				if(saved){
@@ -39,10 +43,9 @@ module.exports = {
 				} else {
 					helpers.errorHandler('Email was not set properly', req, res);
 				}
-			})
+			});
 		} else {
 			helpers.errorHandler('Email was not set properly', req, res);
 		}
-
 	}
-}
+};
