9472e5f7211d9fbc2d27d8626466a267b3a1f2fa
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-02 07:13:02 +0300

    Styling guide i s up working

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,18 @@ var uglify = require('gulp-uglify');
 
 
 var paths = {
-	server : './server/**/*.js'
+	server : './server/server.js'
 }
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['lint','watch']);
 
 gulp.task('watch',function(){
-	gulp.watch(paths.server)
+	gulp.watch(paths.server, ['lint']);
+})
+
+
+gulp.task('lint', function(){
+  return gulp.src(['./server/**/*.js'])
+             .pipe(jshint())
+             .pipe(jshint.reporter('default'))
 })
