75d65938ad6f220e0aaea31d3e0236bab77f4502
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-03 08:58:29 +0300

    Took out client express static

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -7,5 +7,5 @@ module.exports = function(app,express){
 	app.use(morgan('dev'));
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
-	app.use(express.static(__dirname + '/../../client'));	
+	// app.use(express.static(__dirname + '/../../client'));	
 };
