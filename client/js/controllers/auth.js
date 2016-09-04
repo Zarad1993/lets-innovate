5d4bb9ec3f39d88c12a89cf9709d5ce77ca60560
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-04 13:24:09 +0300

    Added OAuth functionality and up working

--- client/js/controllers/auth.js
+++ client/js/controllers/auth.js
@@ -1,16 +1,22 @@
 angular.module('innovate.auth',[])
-.controller('AuthController', function($scope,$location,$window){
-	OAuth.popup('github')
-	.done(function(result) {
-	  //use result.access_token in your API request 
-	  //or use result.get|post|put|del|patch|me methods (see below)
-	  console.log(result);
-	  result.me().done(function(data){
-	  	console.log(data);
-	  })
-	})
-	.fail(function (err) {
-		console.log(err)
-	  //handle error with err
-	});
+.controller('AuthController', function($scope, $location, $window, Client){
+
+	$scope.signWithAuth = function(provider){
+		OAuth.popup(provider)
+		.then(function(result) {
+			console.log(result);
+			$window.localStorage.setItem('com.innov', result.access_token);
+		  result.me().then(function(data){
+		  	console.log(data);
+			setTimeout(function(){
+				$location.path('/');
+				$scope.$apply();
+			},2000);
+		  })
+		})
+		.fail(function (err) {
+			console.log(err)
+		  //handle error with err
+		});				
+	}	
 })
