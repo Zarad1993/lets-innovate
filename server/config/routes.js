3842463352af47a1ab706bd8c9a91757b43c4dfd
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-04 16:12:40 +0300

    Remodifyed routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,17 +1,21 @@
 'use strict';
 // Require all the controller functions here 
 var helpers = require('./helpers.js');
-var clientController = require('./../Client/clientController.js');
+var adminController = require('./../Admin/adminController.js');
 var featureController = require('./../Features/featureController.js');
+var clientController = require('./../Client/clientController.js');
 
 module.exports = function(app){
-	app.post('/api/innov/signin', clientController.signin);
-	app.post('/api/innov/signup', clientController.signup);
-	app.get('/api/innov/clients', clientController.getClients);
+	app.post('/api/innov/signin', adminController.signin);
+	app.post('/api/innov/signup', adminController.signup);
+
 
 	app.post('/api/innov/add', featureController.addNewFeature);
 	app.get('/api/innov/features', featureController.getAllFeatures);
 
+
+	app.get('/api/innov/clients', clientController.getClients);
+	app.post('/api/innov/client', clientController.getOneClient);
 
     // If a request is sent somewhere other than the routes above,
     // send it through our custom error handler
