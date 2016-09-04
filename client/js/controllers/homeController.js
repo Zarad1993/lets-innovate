451e49069be56082368d06c47ca8871539787658
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-04 13:24:32 +0300

    Added initializing function to improve quality code

--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -3,13 +3,21 @@ angular.module('innovate.home',[])
 .controller('HomeController', function($scope, Client, Features){	
 	$scope.clients;
 	$scope.feature = {};
-	Client.getClients()
-		  .then(function(response){
-		  	$scope.clients = response.data;
-		  })
-		  .catch(function(error){
-		  	console.log(error);
-		  })
+
+	
+	$scope.startUp = function(){
+		Client.getClients()
+			  .then(function(response){
+			  	$scope.clients = response.data;
+			  })
+			  .catch(function(error){
+			  	console.log(error);
+			  })
+	}
+
+	$scope.logout = function(){
+		Client.signout();
+	}
 	$scope.sendRequest = function(){
 		Features.addFeature($scope.feature)
 				.then(function(response){
