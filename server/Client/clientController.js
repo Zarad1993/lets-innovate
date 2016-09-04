8f3d743ca14cd7c09498712f1011dc100a845bb1
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-04 16:11:08 +0300

    Rebuilt the database upon OAuth to its needs

--- server/Client/clientController.js
+++ server/Client/clientController.js
@@ -1,6 +1,5 @@
 'use strict';
 var Client = require('./clientModel.js');
-var jwt = require('jwt-simple');
 var helpers = require('./../config/helpers.js');
 
 module.exports = {
@@ -16,50 +15,23 @@ module.exports = {
 			  })
 	},
 
-	signin : function(req,res){
-		var clientEmail = req.body.email;
-		var password = req.body.password;
-
-		Client.findOne({email : clientEmail})
-			.exec(function(err, client){
-				if(!client){
-					helpers.errorHandler('Email or Password is Incorrect',req,res);
-				} else{
-         	        Client.comparePassword(password,client.password, res, function(found){
-	    		        if(found){
-	   				       var token = jwt.encode(client, 'secret');
-	     			        res.setHeader('x-access-token',token);
-	     			        //modified the response to send the username
-	     			        //to save it in local stoarge to be accessed late
-	                        res.json({token:token, email: clientEmail});
-	  			        } else {
-	   				       helpers.errorHandler('Wrong Password', req, res);
-	                    }
-	                });
-				}
-            });	
-	}, 
-
-	signup : function(req,res){
-		var clientName = req.body.name;
-		var clientEmail = req.body.email;
-		var password = req.body.password;
-		
-		var newClient = new Client({
-			name : clientName,
-			email : clientEmail,
-			password : password
-		});
-		if(password && clientEmail){
-			newClient.save(function(err,saved){
-				if(saved){
-					res.status(201).send('New User is Created');
-				} else {
-					helpers.errorHandler('Email was not set properly', req, res);
-				}
-			});
-		} else {
-			helpers.errorHandler('Email was not set properly', req, res);
-		}
+	
+	getOneClient : function(req, res){
+		Client.findOne({email : req.body.email})
+			  .exec(function(err, client){
+			  	if(client){
+			  		res.status(200).send(client)
+			  	} else {
+			  		var newClient = new Client({
+			  			name : req.body.name,
+			  			email : req.body.email
+			  		})
+			  		newClient.save(function(err,saved){
+			  			if(saved){
+			  				res.status(200).send(saved);
+			  			}
+			  		})
+			  	}
+			  })
 	}
 };
--- server/Client/clientModel.js
+++ server/Client/clientModel.js
@@ -1,50 +1,12 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt-nodejs');
-var SALT_WORK_FACTOR = 10;
 
 var Schema = mongoose.Schema;
 
-
 var clientSchema = new Schema({
-  name : {type : String}, 
-	email : {type : String , index : {unique : true}},
-	password : {type : String}
-})
-
-clientSchema.pre('save', function(next){
-  var client = this;
-  // only hash the password if it has been modified (or is new)
-  if (!client.isModified('password')) {
-    return next();
-  }
-// generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-    if (err) {
-      return next(err);
-    }
-    // hash the password along with our new salt
-    bcrypt.hash(client.password, salt, null, function (err, hash) {
-      if (err) {
-        return next(err);
-      }
-      // override the cleartext password with the hashed one
-      client.password = hash;
-      client.salt = salt;
-      next();
-    });
-  });	
+  name : {type : String , required: true}, 
+	email : {type : String , index : {unique : true} , required:true}
 })
 
 var Client = mongoose.model('Client', clientSchema);
-
-Client.comparePassword = function(candidatePassword, savedPassword, res, cb){
-  bcrypt.compare( candidatePassword, savedPassword, function(err, isMatch){
-    if(err){
-      res.status(500).send('Wrong Password');
-    } else if(cb){
-      cb(isMatch);
-    }
-  });
-};
 
 module.exports = Client;
