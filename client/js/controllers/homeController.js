0689d71542f84773bb3d3d8b4c1d6d3bcae65a00
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-04 16:09:16 +0300

    Getting client upon signing in

--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -1,26 +1,28 @@
 angular.module('innovate.home',[])
 
-.controller('HomeController', function($scope, Client, Features){	
-	$scope.clients;
+.controller('HomeController', function($scope, Client, Features, $window){	
 	$scope.feature = {};
 
-	
+
 	$scope.startUp = function(){
-		Client.getClients()
+		$scope.client = {};
+		Client.checkClient({email:$window.localStorage.getItem('com.email')})
 			  .then(function(response){
-			  	$scope.clients = response.data;
+			  	$scope.client.name = response.data.name;
 			  })
 			  .catch(function(error){
 			  	console.log(error);
-			  })
+			  })	
 	}
-
 	$scope.logout = function(){
 		Client.signout();
 	}
 	$scope.sendRequest = function(){
+		console.log('coming here');
 		Features.addFeature($scope.feature)
 				.then(function(response){
+					console.log(response);
+					$window.location.reload();
 					alert('A message has been sent to the client');
 				})
 				.catch(function(error){
