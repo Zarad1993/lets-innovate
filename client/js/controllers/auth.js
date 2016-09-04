aa8b5113118fb8b7e39c23776008e183baccfef0
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-04 16:08:40 +0300

    Keeping client on home page if logged in

--- client/js/controllers/auth.js
+++ client/js/controllers/auth.js
@@ -6,22 +6,34 @@ angular.module('innovate.auth',[])
 			$location.path('/');
 		}
 	}
+
 	$scope.signWithAuth = function(provider){
 		OAuth.popup(provider)
 		.then(function(result) {
-			console.log(result);
 			$window.localStorage.setItem('com.innov', result.access_token);
 		  result.me().then(function(data){
-		  	console.log(data);
-			setTimeout(function(){
-				$location.path('/');
-				$scope.$apply();
-			},2000);
+		  	$scope.addClient(data);
 		  })
 		})
 		.fail(function (err) {
 			console.log(err)
-		  //handle error with err
 		});				
-	}	
+	};
+
+	$scope.addClient = function(data){
+		Client.checkClient({name : data.name, email : data.email})
+			  .then(function(response){
+			  	console.log(response);
+				setTimeout(function(){
+					$window.localStorage.setItem('com.email',data.email);
+					$location.path('/');
+					$scope.$apply();
+				},2000);
+			  })
+			  .catch(function(error){
+			  	console.log(error);
+			  })
+	}
+
+		
 })
