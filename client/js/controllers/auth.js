9386def8d336812e5c2470cc69e1d3122d95155d
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-04 12:06:02 +0300

    Working on oAuth.io

--- client/js/controllers/auth.js
+++ client/js/controllers/auth.js
@@ -1,10 +1,13 @@
 angular.module('innovate.auth',[])
 .controller('AuthController', function($scope,$location,$window){
-	OAuth.popup('facebook')
+	OAuth.popup('github')
 	.done(function(result) {
 	  //use result.access_token in your API request 
 	  //or use result.get|post|put|del|patch|me methods (see below)
 	  console.log(result);
+	  result.me().done(function(data){
+	  	console.log(data);
+	  })
 	})
 	.fail(function (err) {
 		console.log(err)
