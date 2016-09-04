03ccf48ad1e4d28ba765c41b626487ee2f759ef4
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-04 16:07:34 +0300

    Added new admin page for controlled feature Requests

--- client/app.js
+++ client/app.js
@@ -2,23 +2,34 @@ angular.module('lets-innovate',[
 	'innovate.home',
 	'innovate.services',
 	'innovate.auth',
+	'innovate.admin',
 	'ngRoute'
 	])
 .config(function($routeProvider, $httpProvider){
 	$routeProvider
 		.when('/',{
+			cache : false,
 			templateUrl : 'js/templates/home.html',
 			controller : 'HomeController'
 		})
 		.when('/signin',{
+			cache : false,
 			templateUrl : 'js/templates/signin.html',
 			controller : 'AuthController'
 		})
+		.when('/admin',{
+			cache : false,
+			templateUrl : 'js/templates/adminLogin.html',
+			controller: 'AdminController'
+		})
+	    .otherwise({
+    	  redirectTo: '/'
+    	});
 })
 .run(function($rootScope, $location, Client){
 	OAuth.initialize('L47TybIQ1vyV7pZ-2OpJ5NNb3Qo');
 	$rootScope.$on('$routeChangeStart', function (evt, next, current) {
-		if(!Client.isAuth() && next.$$route){
+		if(!Client.isAuth() && next.$$route.originalPath !== '/admin'){
 			$location.path('/signin');
 		}
 	});	
