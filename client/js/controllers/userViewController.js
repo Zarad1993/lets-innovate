2949a64fee0282ec384787e7f721eb14782f8e0f
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-05 22:00:41 +0300

    Added a feature that delete's requests

--- client/js/controllers/userViewController.js
+++ client/js/controllers/userViewController.js
@@ -1,10 +1,24 @@
 angular.module('innovate.user',[])
 .controller('UserViewController',function($scope, $window, Features, $location){
 	var email = $location.path().substr(12,$location.path().length);
+	$scope.features = [];
 
-	
-	Features.getByEmail(email)
-		  .then(function(response){
-		  	console.log(response);
-		  })
+	$scope.runUp = function(){
+		Features.getByEmail(email)
+			  .then(function(response){
+			  	$scope.features = response.data;
+			  	for(var i = 0; i < $scope.features.length; i++){
+			  		$scope.features[i].targetDate = $scope.features[i].targetDate.substr(0,$scope.features[i].targetDate.indexOf('T'));
+			  	}
+			  })		
+	}
+
+	$scope.delete = function(priority,client){
+		Features.deleteFeature({priority : priority , client : client})
+				.then(function(response){
+					console.log(response);
+					$window.location.reload();
+				})
+	}
+
 });
