c3f4da740d9e866ff8d3be59a7b7e2fbcc45c965
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-06 18:04:09 +0300

    Permission for notification

--- client/app.js
+++ client/app.js
@@ -39,6 +39,29 @@ angular.module('lets-innovate',[
 })
 .run(function($rootScope, $location, Client, $window){
 	OAuth.initialize('L47TybIQ1vyV7pZ-2OpJ5NNb3Qo');
+	function notifyMe() {
+		// Let's check if the browser supports notifications
+		if (!("Notification" in window)) {
+			alert("This browser does not support desktop notification");
+		}
+
+		// Let's check whether notification permissions have already been granted
+		else if (Notification.permission === "granted") {
+		// If it's okay let's create a notification
+			// NOTHING HERE
+		}
+
+		// Otherwise, we need to ask the user for permission
+		else if (Notification.permission !== 'denied') {
+			Notification.requestPermission(function (permission) {
+			  // If the user accepts, let's create a notification
+			  if (permission === "granted") {
+			    //NOTHING HERE
+			  }
+			});
+		}
+	}
+
 	$rootScope.$on('$routeChangeStart', function (evt, next, current) {
 		if(!Client.isAuth() && next.$$route.originalPath !== '/admin'){
 			$location.path('/signin');
--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -21,8 +21,11 @@ angular.module('innovate.home',[])
 	$scope.sendRequest = function(){
 		Features.addFeature($scope.feature)
 				.then(function(response){
-					notifyMe();
-					setTimeout(function(){$window.location.reload();},3000);
+					$window.location.reload();
+					if (Notification.permission === "granted") {
+					// If it's okay let's create a notification
+						spawnNotification('Thank You for submitting your Feature Request, It is very valuable for us and it will be taken into consideration upon the priority number','http://payperhead.com/wp-content/uploads/2013/05/Features.jpg','Dear '+$scope.client.name);
+					}
 				})
 				.catch(function(error){
 					console.log(error);
@@ -36,33 +39,6 @@ angular.module('innovate.home',[])
 		    	    var n = new Notification(theTitle,options);
 					setTimeout(n.close.bind(n), 3000);
 				}
-				// Get Request for Notification
-				function notifyMe() {
-					// Let's check if the browser supports notifications
-					if (!("Notification" in window)) {
-						alert("This browser does not support desktop notification");
-					}
-
-					// Let's check whether notification permissions have already been granted
-					else if (Notification.permission === "granted") {
-					// If it's okay let's create a notification
-						spawnNotification('Thank You for submitting your Feature Request, It is very valuable for us and it will be taken into consideration upon the priority number','http://payperhead.com/wp-content/uploads/2013/05/Features.jpg','Dear '+$scope.client.name);
-					}
-
-					// Otherwise, we need to ask the user for permission
-					else if (Notification.permission !== 'denied') {
-					Notification.requestPermission(function (permission) {
-					  // If the user accepts, let's create a notification
-					  if (permission === "granted") {
-					  		spawnNotification('Thank You for submitting your Feature Request, It is very valuable for us and it will be taken into consideration upon the priority number','http://payperhead.com/wp-content/uploads/2013/05/Features.jpg','Dear '+$scope.client.name);
-					  }
-					});
-}
-
-				  // At last, if the user has denied notifications, and you 
-				  // want to be respectful there is no need to bother them any more.
-				}				
-
 	};
 
 	$scope.myTickets = function(email){
