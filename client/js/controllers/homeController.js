d13969dc3f55bd05527bb18801ffc15b4f770699
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-06 17:49:36 +0300

    Added notification upon submitting a new feature request

--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -19,14 +19,50 @@ angular.module('innovate.home',[])
 		Client.signout();
 	}
 	$scope.sendRequest = function(){
-		console.log('coming here');
 		Features.addFeature($scope.feature)
 				.then(function(response){
 					$window.location.reload();
+					notifyMe();
 				})
 				.catch(function(error){
 					console.log(error);
 				});
+				// Display Notification
+				function spawnNotification(theBody,theIcon,theTitle) {
+					var options = {
+						body: theBody,
+						icon: theIcon
+					}
+		    	    var n = new Notification(theTitle,options);
+					setTimeout(n.close.bind(n), 3000);
+				}
+				// Get Request for Notification
+				function notifyMe() {
+					// Let's check if the browser supports notifications
+					if (!("Notification" in window)) {
+						alert("This browser does not support desktop notification");
+					}
+
+					// Let's check whether notification permissions have already been granted
+					else if (Notification.permission === "granted") {
+					// If it's okay let's create a notification
+						spawnNotification('Thank You for submitting your Feature Request, It is very valuable for us and it will be taken into consideration upon the priority number','http://payperhead.com/wp-content/uploads/2013/05/Features.jpg','Dear '+$scope.client.name);
+					}
+
+					// Otherwise, we need to ask the user for permission
+					else if (Notification.permission !== 'denied') {
+					Notification.requestPermission(function (permission) {
+					  // If the user accepts, let's create a notification
+					  if (permission === "granted") {
+					  		spawnNotification('Thank You for submitting your Feature Request, It is very valuable for us and it will be taken into consideration upon the priority number','http://payperhead.com/wp-content/uploads/2013/05/Features.jpg','Dear '+$scope.client.name);
+					  }
+					});
+}
+
+				  // At last, if the user has denied notifications, and you 
+				  // want to be respectful there is no need to bother them any more.
+				}				
+
 	};
 
 	$scope.myTickets = function(email){
