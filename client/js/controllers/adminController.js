452812f764d3093f630a917aaa66bc687a10acd3
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-08 20:54:56 +0300

    Added documentation for admin controller

--- client/js/controllers/adminController.js
+++ client/js/controllers/adminController.js
@@ -1,9 +1,10 @@
 'use strict';
 angular.module('innovate.admin',[])
 .controller('AdminController',function($scope, Admin, $window, $location, Client, Features){
-	$scope.user ={};
-	$scope.clients = [];
-	$scope.numberOfRequests = 0;
+	$scope.user ={};  // user -- > admin
+	$scope.clients = []; // clients in application
+	$scope.numberOfRequests = 0; // Number of requests in total
+	// Sign in Function
 	$scope.login = function(){
 		Admin.signin($scope.user)
 			 .then(function(response){
@@ -15,14 +16,19 @@ angular.module('innovate.admin',[])
 			 });
 	};
 
+	// Calling the same controller in different route
+
 	if($location.path() === '/admin/home'){
 		$scope.sortType     = 'name'; // set the default sort type
 		$scope.sortReverse  = false;  // set the default sort order
 
+		// Get All clients in Database
 		Client.getClients()
 			  .then(function(response){
 			  	$scope.clients = response.data;
 			  });
+
+		// Get Total number of requests
 		Features.getFeatures()
 				.then(function(response){
 					$scope.numberOfRequests = response.data.length;
@@ -32,10 +38,12 @@ angular.module('innovate.admin',[])
 				});
 	}
 
+	// Display client's Data
 	$scope.showClient = function(data){
 		$location.path('/admin/home/'+data);
 	};
 
+	// Logout
 	$scope.logout = function(){
 		Client.signout();
 	};
