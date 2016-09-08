488a998aa1a56e635e2e4402aa03d0a458e78fb4
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-08 14:08:57 +0300

    Uglified all scripts for front-end rendering optimization

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,20 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var ignore = require('gulp-ignore');
+var pump = require('pump');
+var concat = require('gulp-concat');
+
 
 
 var paths = {
 	server : ['./server/**/*.js','./client/js/**/*.js','./client/app.js']
 }
 
-gulp.task('default', ['lint','watch']);
+gulp.task('default', ['lint','scripts','watch']);
 
 gulp.task('watch',function(){
 	gulp.watch(paths.server, ['lint']);
-})
+});
 
 
 gulp.task('lint', function(){
@@ -20,4 +23,19 @@ gulp.task('lint', function(){
     		   .pipe(ignore.exclude(/Spec\.js/))
                .pipe(jshint('.jshintrc'))
                .pipe(jshint.reporter('default'))
-})
+});
+
+gulp.task('compress', function(cb){
+	pump([
+		gulp.src('./client/lib/**/*.js'),
+		uglify(),
+		gulp.dest('client/dist')
+		],cb)
+});
+
+gulp.task('scripts', function() {
+  gulp.src(['./client/lib/oauth-js/dist/oauth.js', './client/lib/angular/angular.min.js', './client/lib/angular-ui/build/angular-ui.min.js', './client/lib/angular-route/angular-route.js'])
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./client/dist/'))
+});
