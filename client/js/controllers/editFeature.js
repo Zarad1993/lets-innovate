9d26ac2cebffc35f54b410b78aeeaabd97cde463
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-08 21:02:18 +0300

    Added documentation for edit Feature controller

--- client/js/controllers/editFeature.js
+++ client/js/controllers/editFeature.js
@@ -2,16 +2,24 @@
 angular.module('innovate.edit',[])
 .controller('EditFeatureController', function($scope, $window, $location, Client, Features){
 
+	// This is the Edit Controller where only the User-(CLIENT) is allowed
+	// to edit (Update) his feature request
+
+
+	// Get the email from the path
 	var email = $location.path().substr(12,$location.path().length);
+	// Get priority from path
 	var priority = $location.path().split('/');
 	email = priority [priority.length-2];
 	priority = priority[priority.length-1];
 	$scope.feature = {};
 
+	// on Running the controller
+	// We call the feature our user clicked on 
 	$scope.start = function(){
 		Features.getOneFeature({email : email,priority : priority})
 				.then(function(response){
-					$scope.feature = response.data;
+					$scope.feature = response.data; // feature data rendered on DOM 
 					$scope.feature.targetDate = new Date();
 				})
 				.catch(function(error){
@@ -19,6 +27,9 @@ angular.module('innovate.edit',[])
 				});
 	};
 
+
+	// Upon finishing then pass a notification by success 
+	// and save data in database
 	$scope.submitEdit = function(){
 		function spawnNotification(theBody,theIcon,theTitle) {
 			var options = {
@@ -46,6 +57,8 @@ angular.module('innovate.edit',[])
 					console.log(error);
 				});
 	};
+
+	// This is a simple tweak that plays a small notification sound
 	$scope.playSound = function(){   
 	    document.getElementById('sound').innerHTML='<audio autoplay="autoplay"><source src="sounds/filling-your-inbox.mp3" type="audio/mpeg" /></audio>';
 	};
