c9d539a2a7b95d02be320653373d90d39f3d68d6
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-09-11 05:15:50 +0300

    Added a notification with calling new ordered features to be re ordered upon cient's priorities

--- client/js/controllers/allFeaturesController.js
+++ client/js/controllers/allFeaturesController.js
@@ -1,5 +1,5 @@
 angular.module('innovate.features',[])
-.controller('AllFeatureController', function($scope, Features, $location, $window, Client){
+.controller('AllFeatureController', function($scope, Features, $location, $window, Client, socket){
 	Features.getFeatures()
 			.then(function(response){
 				$scope.features = response.data;
@@ -11,5 +11,32 @@ angular.module('innovate.features',[])
 
 	$scope.logout = function(){
 		Client.signout();
-	}
+	};
+
+	$scope.playSound = function(){   
+	    document.getElementById('sound').innerHTML='<audio autoplay="autoplay"><source src="sounds/filling-your-inbox.mp3" type="audio/mpeg" /></audio>';
+	};
+
+
+
+	socket.on('reload', function(data){
+		// Display Notification
+		function spawnNotification(theBody,theIcon,theTitle) {
+			var options = {
+				body: theBody,
+				icon: theIcon
+			};
+    	    var n = new Notification(theTitle,options);
+			setTimeout(n.close.bind(n), 3000);
+		}
+
+		Features.getFeatures()
+				.then(function(response){
+					if(response.status === 200){
+						$scope.playSound();
+						$scope.features = response.data;
+						spawnNotification(data + ' have updated his priorities','http://payperhead.com/wp-content/uploads/2013/05/Features.jpg','Dear Admin');
+					}
+		})
+	});
 })
